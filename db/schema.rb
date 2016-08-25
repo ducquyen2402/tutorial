43ddf770838cfa96398f1b1a4f700d652f2c0325
ninh.duc.quyen <ninh.duc.quyen@gmail.com>
2016-08-25 11:19:42 +0700

    account-activationn

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,5 @@
-ActiveRecord::Schema.define(version: 20160827213031) do
+ActiveRecord::Schema.define(version: 20160828194745) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -7,6 +8,9 @@ ActiveRecord::Schema.define(version: 20160827213031) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
